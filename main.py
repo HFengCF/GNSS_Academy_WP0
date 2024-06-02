896298fcf39f804a6df640c479c1556a9e0d8a31
Hao Feng <fengchenfu1@gmail.com>
2024-06-02 21:50:25 +0200

    Reading files

--- main.py
+++ main.py
@@ -7,12 +7,36 @@ import numpy as np
 import pandas as pd
 
 
+def read_fields_file(path_name):
+    dict = {}
+    with open(path_name, 'r') as f:
+        # Read file
+        lines = f.readlines()
+        
+        for line in lines:
+            if "#" in line:
+                line_splited = line.replace("#", "").split()
+                number_fields = len(line_splited)
+                
+                for i in range(1, number_fields):
+                    dict[line_splited[i]] = []
+    return dict
 
-
-
+def fulfill_dict_fields(dict, path_name):
+    with open(path_name, 'r') as f:
+        # Read file
+        lines = f.readlines()
+        
+        for line in lines:
+            if "#" not in line:
+                line_splited = line.split()
+                
+    return dict
+ 
 
 
 
 
 if __name__ == "__main__":
-    pass
+   path_name = f"C:/Users/fengc/OneDrive/Documentos/WP0_RCVR_ANALYSIS/SCEN/SCEN_TLSA00615-GPSL1-SPP/OUT/LOS/TLSA00615_LosInfo_5s.dat"
+   read_fields_file(path_name)
